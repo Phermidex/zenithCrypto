f2aff1288cbed6b52084152d2c309fdcc74dfaf7
Hermides Medina (Miguel) <hermides07@gmail.com>
2026-02-15 23:39:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6203767109-b1071",
+  "appId": "1:965246187365:web:49fcf42fa1d6ddf489538c",
+  "apiKey": "AIzaSyBOA9DwQU0Zkew2O_fkkOhzMikoU8ZP79s",
+  "authDomain": "studio-6203767109-b1071.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "965246187365"
+};
